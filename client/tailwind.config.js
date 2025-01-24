9d1da9260c1711ee7424a7d20a00c5b13804f9da
Sachin <sachsaini2004@gmail.com>
2025-01-24 15:37:28 +0530

    Add event card background color to CSS and Tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -29,6 +29,9 @@ theme: {
 			'text-green': '#006833',
 			background: 'hsl(var(--background))',
 			foreground: 'hsl(var(--foreground))',
+			eventcard: {
+				background: 'hsl(var(eventcard-background))',
+			},
 			card: {
 				DEFAULT: 'hsl(var(--card))',
 				overlay:{
