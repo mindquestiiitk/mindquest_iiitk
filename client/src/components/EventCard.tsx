86267a503c95f5f4a2c8c42471554bb85c735667
Sarthak Gupta <sarthakg4953@gmail.com>
2025-01-31 00:34:32 +0530

    Update font styles and sizes in EventCard component for improved readability

--- client/src/components/EventCard.tsx
+++ client/src/components/EventCard.tsx
@@ -13,7 +13,7 @@ export function EventCard({ id, title, image, brief }: EventCardProps) {
 
   return (
     <div
-      className="relative overflow-hidden font-acme group transform transition-all duration-300 hover:scale-105  bg-eventcard-background border-card-overlay-background rounded-lg"
+      className="relative overflow-hidden font-montserrat group transform transition-all duration-300 hover:scale-105  bg-eventcard-background border-card-overlay-background rounded-lg"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -31,8 +31,8 @@ export function EventCard({ id, title, image, brief }: EventCardProps) {
           className={`absolute inset-0 bg-card-overlay-background/50 bg-opacity-100 flex flex-col justify-center items-center p-4 transition-all duration-300 ${isHovered ? "opacity-100 translate-y-0" : "opacity-100 md:opacity-0 translate-y-0 md:translate-y-4"
             }`}
         >
-          <h3 className="text-white text-2xl font-bold mb-2 transform transition-all duration-300">{title}</h3>
-          <p className="text-white text-base font-light mb-4 text-center transform transition-all duration-300">{brief}</p>
+          <h3 className="text-white text-xl font-bold mb-2 transform transition-all duration-300">{title}</h3>
+          <p className="text-white text-sm font-light mb-4 text-center transform transition-all duration-300">{brief}</p>
           <Link to={`/events/${id}`} className="transform transition-all duration-300 hover:scale-105">
             <button className="px-4 py-2 bg-white md:bg-[#D4F3D9] text-card-overlay-background hover:bg-white  hover:text-card-overlay-background transition-all duration-300 rounded-md">
               Know More
