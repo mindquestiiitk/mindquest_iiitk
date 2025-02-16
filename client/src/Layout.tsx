c89f0dd533f4666a6fa31625e5ce82143fe2a977
Sarthak Gupta <sarthakg4953@gmail.com>
2025-02-16 14:14:47 +0530

    Add Vercel Analytics and Speed Insights to the project

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -2,10 +2,14 @@
 import { Outlet } from 'react-router-dom'
 import Navbar_Home from './components/Navbar/Navbar_Home'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Layout = () => {
   return (
     <>  
+      <Analytics />
+      <SpeedInsights />
       <div className='relative flex flex-col min-h-screen'>
         <div className="z-30 w-full pb-2">
           <Navbar_Home />
