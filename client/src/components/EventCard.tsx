2cc6ba12c4fc45616f6163d2dd2a6dba7f99fe3b
Sarthakyadav98 <s9808927657@gmail.com>
2025-03-01 17:44:32 +0530

    add events

--- client/src/components/EventCard.tsx
+++ client/src/components/EventCard.tsx
@@ -8,6 +8,7 @@ interface EventCardProps {
   images: string[]
   poster: string
   brief: string
+  thumbnail: string
 }
 
 export function EventCard({ id, title, images, poster, brief }: EventCardProps) {
