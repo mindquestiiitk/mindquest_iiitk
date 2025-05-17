15732b7fd6b22e8c9dce17c8c1aa32e9cfc3a943
Sarthak Gupta <sarthakg4953@gmail.com>
2025-05-17 13:33:25 +0530

    Remove unused DataItem type definition from utils.ts

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,11 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-type DataItem = {
-  id: number;
-  date: string; // Format: DD/MM/YYYY
-};
-
 // Helper to convert DD/MM/YYYY to Date
 export const parseDate = (str: string): Date => {
   const [day, month, year] = str.split('/').map(Number);
