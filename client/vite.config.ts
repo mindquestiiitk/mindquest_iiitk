3a524864307f26f7a62abfcedafecfe7a3163fa7
Sarthak Gupta <sarthakg4953@gmail.com>
2025-05-22 15:31:31 +0530

    feat: integrate Vercel support and add API handler
    - Added Vercel plugin to Vite configuration for deployment support.
    - Defined global variable __APP_ENV__ to manage environment settings.
    - Created a new API handler to respond with request details (body, query, cookies).
    - Added a Vercel configuration file for deployment settings.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,18 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+   // define __APP_ENV to be used in the app
+  define: {
+    __APP_ENV__: JSON.stringify(process.env.VITE_APP_ENV || "development"),
   },
 })
